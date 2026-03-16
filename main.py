5b84dd1c3b97eec7326ac7498b454d52871ff27e
Tim Reynolds <reynolds.tim@protonmail.com>
2026-03-15 20:51:16 -0500

    destruction

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ def main() -> None:
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
